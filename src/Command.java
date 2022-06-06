ab706dfb4a38663ca10ba20a2550e3f32f1b5396
Kadir OZ <kadirzdev@gmail.com>
2022-06-06 16:59:12 +0300

    Create Command.java
    CommandPattern Added

---
+++ src/Command.java
@@ -0,0 +1,57 @@
+interface resetDevice {
+    public void shutdown();
+}
+class HardDiskReset implements resetDevice {
+    public void shutdown() {
+        devices.Reset(HardDisk);
+    }
+    private Devices devices;
+    private Devices HardDisk;
+}
+class CPUReset implements resetDevice {
+    public void shutdown() {
+        devices.Reset(CPU);
+    }
+    private Devices devices;
+    private Devices CPU;
+}
+class IOReset implements resetDevice {
+    public void shutdown() {
+        devices.Reset(IODevices);
+    }
+    private Devices devices;
+    private Devices IODevices;
+}
+class OperatingSystem {
+    public void Reset(resetDevice resetDevice) {
+        resetDevice.shutdown();
+        devices.Reset(HardDisk);
+        devices.Reset(CPU);
+        devices.Reset(IODevices);
+
+    }
+    private Devices devices;
+    private Devices HardDisk;
+    private Devices CPU;
+    private Devices IODevices;
+}
+
+class GenericReset implements resetDevice{
+    public void shutdown() {
+        devices.Reset(HardDisk);
+        devices.Reset(CPU);
+        devices.Reset(IODevices);
+    }
+    private Devices devices;
+    private Devices HardDisk;
+    private Devices CPU;
+    private Devices IODevices;
+
+
+}
+
+public class Command {
+    public static void main(String[] args) {
+
+    }
+}
